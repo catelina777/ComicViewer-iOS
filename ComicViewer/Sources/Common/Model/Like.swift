38647db2956d7a5ccd3ba90829822c234ada5217
Ryuhei Kaminishi <catelina0717@gmail.com>
2018-09-30 22:15:34 +0900

    finish #10

--- ComicViewer/Sources/Common/Model/Like.swift
+++ ComicViewer/Sources/Common/Model/Like.swift
@@ -12,10 +12,14 @@ import RealmSwift
 class Like: Object {
 
     @objc dynamic var index = -1
+    @objc dynamic var locX: Double = 0
+    @objc dynamic var locY: Double = 0
     @objc dynamic var date = Date()
 
-    convenience init(index: Int) {
+    convenience init(index: Int, locX: Double, locY: Double) {
         self.init()
         self.index = index
+        self.locX = locX
+        self.locY = locY
     }
 }
